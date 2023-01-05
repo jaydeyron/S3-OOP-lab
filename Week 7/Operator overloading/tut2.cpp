651dd5a16813b7fd91d2ea8d017793f9a250ceec
Jay <94859351+jays56@users.noreply.github.com>
2023-01-05 15:53:36 +0530

    Create tut2.cpp

---
+++ Week 7/Operator overloading/tut2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+	public:
+		int r,i;
+		complex()
+		{
+			cout<<"Enter real part:"<<endl;
+			cin>>r;
+			cout<<"\nEnter imaginary part"<<endl;
+			cin>>i;
+			cout<<endl;
+		}
+		complex(int x,int y)
+		{
+			r=x;
+			i=y;
+		}
+		void print()
+		{
+			cout<<"Result:"<<endl;
+			cout<<r<<"+"<<i<<"i\n";
+		}
+		complex operator -(complex const &obj)
+		{
+			complex res(0,0);
+			res.r=r-obj.r;
+			res.i=i-obj.i;
+			return res;
+		}
+	};
+
+int main()
+{
+	complex a,b,c(0,0);
+	c=a-b;
+	c.print();
+}
