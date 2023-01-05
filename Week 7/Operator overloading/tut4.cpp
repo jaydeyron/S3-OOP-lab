8bc34c0ca6b0dbbcd2d057156ffbcaef00efc7f5
Jay <94859351+jays56@users.noreply.github.com>
2023-01-05 15:58:54 +0530

    Create tut4.cpp

---
+++ Week 7/Operator overloading/tut4.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+	public:
+		float r,i;
+		complex()
+		{
+			cout<<"Enter real part:"<<endl;
+			cin>>r;
+			cout<<"\nEnter imaginary part"<<endl;
+			cin>>i;
+			cout<<endl;
+		}
+		complex(float x,float y)
+		{
+			r=x;
+			i=y;
+		}
+		void print()
+		{
+			cout<<"Result:"<<endl;
+			cout<<r<<"+"<<i<<"i\n";
+		}
+		complex operator *(complex const &obj)
+		{
+			complex res(0,0);
+			res.r=(r*obj.r+i*obj.i)/(obj.r*obj.r+obj.i*obj.i);
+			res.i=(i*obj.r-r*obj.i)/(obj.r*obj.r+obj.i*obj.i);
+			return res;
+		}
+	};
+
+int main()
+{
+	complex a,b,c(0,0);
+	c=a*b;
+	c.print();
+}
