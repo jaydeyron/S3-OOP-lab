747a4c5a06a627de1d09de7323d4a04a989edd6c
Jay <94859351+jays56@users.noreply.github.com>
2023-01-12 16:12:55 +0530

    Create lc9.cpp

---
+++ Lab Cycle Set-4/lc9.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+class string{
+	public:
+		{
+			char s[50];
+			string operator +(string const &obj)
+			{
+				
+			}
+			string operator =(string const &obj)
+			{
+				
+			}
+			string operator -(string const &obj)
+			{
+				
+			}
+			string operator <=(string const &obj)
+			{
+				
+			}
+			string operator ++(string const &obj)
+			{
+				
+			}
+			string operator --(string const &obj)
+			{
+				
+			}
+			void strLength()
+			{
+				for(int i=0;i<n;i++)
+				{
+					
+				}
+			}
+		}
+};
+
+int main()
+{
+	int x1,y1;
+	point p;
+	int ch;
+	while(1)
+	{
+		cout<<"\n\n\t\tSTRING MENU\n\t\t===========\n\n1. Enter string\n\n";
+		cout<<"Enter your choice:\t";
+		cin>>ch;
+		cout<<endl;
+		switch(ch)
+		{
+			case 1:{
+					break;
+				}
+			case 2:{
+					break;
+				}
+			case 3:{
+					break;
+				}
+			case 4:{
+					break;
+				}
+			case 5:{
+					break;
+				}
+			case 6:{
+					break;
+				}
+			case 7:{
+					cout<<"\t\tTHANK YOU\n\n";
+					exit(0);
+				}
+			default:
+				{
+					cout<<"Invalid choice";
+					break;
+				}
+		}
+	}
+}
