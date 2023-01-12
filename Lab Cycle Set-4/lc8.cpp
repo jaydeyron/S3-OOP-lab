17eed62ba8dce64a4ed936ad99fd3999085c8207
Jay <94859351+jays56@users.noreply.github.com>
2023-01-12 16:12:35 +0530

    Create lc8.cpp

---
+++ Lab Cycle Set-4/lc8.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class point{
+	public:
+		int x,y;
+		point()
+		{
+			x=0;
+			y=0;
+		}
+		point(int x,int y)
+		{
+			this->x=x;
+			this->y=y;
+		}
+		void setX(int x)
+		{
+			this->x=x;
+		}
+		void setY(int y)
+		{
+			this->y=y;
+		}
+		void setXY(int x,int y)
+		{
+			this->x=x;
+			this->y=y;
+		}
+		void sum(int a,int b)
+		{
+			int s;
+			s=a+b;
+			cout<<"The sum is "<<s;
+		}
+		void sum(int a)
+		{
+			int r,s;
+			int temp=a;
+			while(temp>0)
+			{
+				r=temp%10;
+				s=s+r;
+				temp=temp/10;
+			}
+			cout<<"Sum of digits of "<<a<<" is "<<s;
+		}
+		/*void sum(int y)
+		{
+			int d2;
+			while(y!=0)
+			{
+				y=y/10;
+				d2=d2+1;
+			}
+			cout<<"Sum of digits of "<<y<<" is "<<d2;
+		}*/
+};
+
+int main()
+{
+	int x1,y1;
+	point p;
+	int ch;
+	while(1)
+	{
+		cout<<"\n\n\t\tPOINT MENU\n\t\t===========\n\n1. Enter x\n2. Enter y\n3. Enter x and y\n4. Find sum of x and y\n5. Sum of digits of x\n6. Sum of digits of y\n7.Exit\n\n";
+		cout<<"Enter your choice:\t";
+		cin>>ch;
+		cout<<endl;
+		switch(ch)
+		{
+			case 1:{
+					//int x1;
+					cout<<"Enter number:\t";
+					cin>>x1;
+					p.setX(x1);
+					//cout<<p.x;
+					break;
+				}
+			case 2:{
+					//int y1;
+					cout<<"Enter number:\t";
+					cin>>y1;
+					p.setY(y1);
+					break;
+				}
+			case 3:{
+					cout<<"\nEnter number:\t";
+					cin>>x1;
+					cout<<"\nEnter second number:\t";
+					cin>>y1;
+					p.setXY(x1,y1);
+					break;
+				}
+			case 4:{
+					cout<<p.x<<"+"<<p.y<<endl;
+					p.sum(p.x,p.y);
+					break;		
+				}
+			case 5:{
+					//cout<<x1<<endl;
+					p.sum(p.x);
+					break;	
+				}
+			case 6:{
+					//cout<<y1<<endl;
+					p.sum(p.y);
+					break;
+				}
+			case 7:{
+					cout<<"\t\tTHANK YOU\n\n";
+					exit(0);
+				}
+			default:
+				{
+					cout<<"Invalid choice";
+					break;
+				}
+		}
+	}
+}
